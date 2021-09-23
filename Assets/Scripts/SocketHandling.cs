670faa552deb730cd13cd6e2b362d358b3dfdf91
Mohamed Sadiq Ikbal <mohamedsadiq.ikbal@gmail.com>
2021-09-23 04:20:52 +0200

    Assembly success registered

--- Assets/Scripts/SocketHandling.cs
+++ Assets/Scripts/SocketHandling.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,19 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class SocketHandling : MonoBehaviour
 {
     [SerializeField] private List<XRSocketInteractor> _socketsInObject;
+    [SerializeField] private int _requiredAssemblyCount = 12;
+    
+    public int assemblyCount;
+    public bool isAssemblySuccess;
+
+    private void Awake()
+    {
+        assemblyCount =0;
+        isAssemblySuccess = false;
+    }
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         foreach (var socket in _socketsInObject)
         {
@@ -16,15 +28,26 @@ public class SocketHandling : MonoBehaviour
         }
         
     }
-    
+
+    private void Update()
+    {
+        if (assemblyCount == _requiredAssemblyCount)
+        {
+            isAssemblySuccess = true;
+            /*Debug.Log("Great Job, you are now a VR Certified Engine Mechanic");   */
+        }
+    }
+
     private void SocketAttached(SelectEnterEventArgs arg0)
     {
         IgnoreCollision(arg0.interactable, true);
+        assemblyCount++;
     }
 
     private void SocketDettached(SelectExitEventArgs arg0)
     {
         IgnoreCollision(arg0.interactable, false);
+        assemblyCount--;
     }
 
 
@@ -33,7 +56,6 @@ public class SocketHandling : MonoBehaviour
         var myColliders = GetComponentsInChildren<Collider>();
         foreach (var myCollider in myColliders)
         {
-            Debug.Log(myCollider.name);
             foreach (var interactableCollider in interactable.colliders)
             {
                 Physics.IgnoreCollision(myCollider, interactableCollider, ignore);
